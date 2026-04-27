c55ea5fb1087120712288525d85d4b11b642c58e
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2026-04-27 20:57:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "../HeroSection/Hero"
 import Nav from "../components/Nav"
+import Menu from "../components/Menu"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Nav />
       <Hero />
+      <Menu />
     </>
   )
 }
